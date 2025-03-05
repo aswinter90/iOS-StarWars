6faf618a49d99a404a6b381fbc25090b554ef535
Arne-Sebastian Winter <aswinter90@googlemail.com>
2025-03-05 09:16:48 +0100

    Initial commit

---
+++ iOS-StarWars/iOS_StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_StarWarsApp.swift
+//  iOS-StarWars
+//
+//  Created by Arne-Sebastian Winter on 05.03.25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
