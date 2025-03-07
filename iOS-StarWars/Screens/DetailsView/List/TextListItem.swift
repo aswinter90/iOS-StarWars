64daef417e1e242337747b029728cb97d679fd07
Arne-Sebastian Winter <aswinter90@googlemail.com>
2025-03-07 20:33:50 +0100

    Use LabeledContent for aligning long texts vertically

--- iOS-StarWars/Screens/DetailsView/List/TextListItem.swift
+++ iOS-StarWars/Screens/DetailsView/List/TextListItem.swift
@@ -8,43 +8,16 @@
 import SwiftUI
 
 struct TextListItem: View {
-    private static let verticalLayoutThreshold: CGFloat = 20
-
     let key: String
     let value: String
 
-    @State private var shouldBeVerticalLayout = false
-
     var body: some View {
-        detailsStack {
-            Group {
-                Text(key.capitalized)
-                    .font(.headline)
-
-                Spacer()
-
-                Text(value)
-                    .font(.subheadline)
-                    .onGeometryChange(for: CGSize.self) { proxy in
-                        proxy.size
-                    } action: { newSize in
-                        shouldBeVerticalLayout = newSize.height > Self.verticalLayoutThreshold
-                    }
-            }
-        }
-    }
-
-    @ViewBuilder
-    private func detailsStack<Content: View>(_ content: () -> Content) -> some View {
-        if shouldBeVerticalLayout {
-            VStack(alignment: .leading) {
-                content()
-            }
-            .padding(.vertical, 4)
-        } else {
-            HStack(alignment: .top) {
-                content()
-            }
+        LabeledContent {
+            Text(value)
+                .font(.subheadline)
+        } label: {
+            Text(key.capitalized)
+                .font(.headline)
         }
     }
 }
